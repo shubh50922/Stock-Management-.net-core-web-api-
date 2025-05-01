10f8cde33593ce2277d16755d5ae1ac50386b3fd
shubhanshi.a2808 <shubhanshi.a@cisinlabs.com>
2025-05-01 19:43:46 +0530

    first commit

---
+++ Controllers/StockController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Stock;
+using api.Mappers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+   [Microsoft.AspNetCore.Mvc.RouteAttribute("api/stock")]
+
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+       public StockController(ApplicationDBContext context)
+       {
+        _context=context;
+        } 
+      [HttpGet]  
+      public IActionResult GetAll(){
+        var stocks=_context.Stock.ToList().Select(s=>s.ToStockDto());
+     
+          
+        return Ok(stocks);
+      }
+    
+     [HttpGet("{id}")]  
+     public IActionResult GetById([FromRoute] int id){
+        var stock=_context.Stock.Find(id);
+        if(stock==null){
+            return NotFound();
+        }
+         return Ok(stock.ToStockDto());
+     }
+
+[HttpPost]
+public IActionResult Create([FromBody] CreateStockRequestDto stockDto){
+   var stockModel=stockDto.ToStockFromCreateDto();
+    _context.Stock.Add(stockModel);
+    _context.SaveChanges();
+    return CreatedAtAction(nameof(GetById),new {id=stockModel.Id},stockModel.ToStockDto());
+   
+}
+
+
+[HttpPut("{id}")]
+public IActionResult Update([FromRoute] )
+}
+}
